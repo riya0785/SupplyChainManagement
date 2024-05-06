49ca4380a0a7fc43b8c05caed492b80ccd954f1f
RIYA SHUKLA <114848829+riya0785@users.noreply.github.com>
2024-05-06 11:48:17 +0530

    Update Orders.java

--- src/inventory/Orders.java
+++ src/inventory/Orders.java
@@ -7,150 +7,131 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import database.DBConnector;
+import database.DataBaseMethods;
 
-public class Orders extends DBConnector {
+// Class to manage orders in the inventory system
+public class Orders extends DataBaseMethods {
 
-	private int orderId, productId, orderQty;
-	private String username, item;
-	private float price;
-	
-	
+    // Instance variables
+    private int orderId, productId, orderQty;
+    private String username, item;
+    private float price;
+    
+    // Default constructor
+    public Orders() {};
 
-	public Orders() {};
+    // Parameterized constructor
+    public Orders(String username, int productId, int orderQty, String item, float price) {
+        this.username = username;
+        this.productId = productId;
+        this.orderQty = orderQty;
+        this.item = item;
+        this.price = price;
+    }
 
-	public Orders(String username, int productId, int orderQty, String item, float price) {
-		this.username = username;
-		this.productId = productId;
-		this.orderQty = orderQty;
-		this.item = item;
-		this.price = price;
-	}
+    // Method to add a new order to the database
+    public void add() {
+        Connection connect = connectInventoryDB(); // Establish database connection
 
-	public void add() {
-		Connection connect = connectInventoryDB();
+        // SQL INSERT query
+        String query = "INSERT into orders (order_id, username, product_id, item, order_qty, price) VALUES (?,?,?,?,?,?)";
+        
+        try (PreparedStatement st = connect.prepareStatement(query)) {
+            // Set values for placeholders in the query
+            st.setInt(1, getOrderId());
+            st.setString(2, getUsername());
+            st.setInt(3, getProductId());
+            st.setString(4, getItem());
+            st.setInt(5, getOrderQty());
+            st.setFloat(6, getPrice());
 
-		String query = "INSERT into orders (order_id, username, product_id, item, order_qty, price) VALUES (?,?,?,?,?,?)";
-		
-		try (PreparedStatement st = connect.prepareStatement(query)) {
-			st.setInt(1, getOrderId());
-			st.setString(2, getUsername());
-			st.setInt(3, getProductId());
-			st.setString(4, getItem());
-			st.setInt(5, getOrderQty());
-			st.setFloat(6, getPrice());
+            int rowsAffected = st.executeUpdate(); // Execute the query
 
-			int rowsAffected = st.executeUpdate();
+            // Check if the order was successfully added
+            if (rowsAffected > 0) {
+                System.out.println("Order Successfully Added");
+            } else {
+                System.out.println("Order Failed");
+            }
+        } catch(SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Method to retrieve and return a list of all orders from the database
+    public List<Object[]> showOrders() {
+        List<Object[]> orderList = new ArrayList<>();
+        Connection connect = connectInventoryDB(); // Establish database connection
+        String query = "SELECT * FROM orders"; // SQL SELECT query
 
-			if (rowsAffected > 0) {
-				System.out.println("Order Successfully Added");
-			} else {
-				System.out.println("Order Failed");
-			}
-		}catch(SQLException e) {
-			e.printStackTrace();
-		}
+        try (PreparedStatement st = connect.prepareStatement(query);
+             ResultSet result = st.executeQuery()) {
+            // Iterate over the result set
+            while (result.next()) {
+                int orderId = result.getInt("order_id");
+                String username = result.getString("username");
+                int productId = result.getInt("product_id");
+                String item = result.getString("item");
+                int quantity = result.getInt("order_qty");
+                float price = result.getFloat("price");
 
-	}
-	
-	public List<Object[]> showOrders() {
-	    List<Object[]> orderList = new ArrayList<>();
-	    Connection connect = connectInventoryDB();
-	    String query = "SELECT * FROM orders";
+                // Create an array containing order details
+                Object[] row = {orderId, username, productId, item, quantity, price};
+                orderList.add(row); // Add the array to the list
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return orderList;
+        }
+        return orderList; // Return the list of orders
+    }
+    
+    // Getter and Setter Methods
+    public int getOrderId() {
+        return orderId;
+    }
 
-	    try (PreparedStatement st = connect.prepareStatement(query);
-	         ResultSet result = st.executeQuery()) {
-	        while (result.next()) {
-	        	int orderId = result.getInt("order_id");
-	        	String username = result.getString("username");
-	            int productId = result.getInt("product_id");
-	            String item = result.getString("item");
-	            int quantity = result.getInt("order_qty");
-	            float price = result.getFloat("price");
+    public void setOrderId(int orderId) {
+        this.orderId = orderId;
+    }
 
-	            Object[] row = {orderId,username, productId, item, quantity, price};
-	            orderList.add(row);
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	        return orderList;
-	    }
-	    return orderList;
+    public int getProductId() {
+        return productId;
+    }
 
-	}
-	
-//	public void show() {
-//		Connection connect = connectInventoryDB();
-//		
-//		String query = "SELECT * from orders";
-//		
-//		ResultSet result = null;
-//		
-//		try (PreparedStatement st = connect.prepareStatement(query)) {
-//			
-//			result = st.executeQuery();
-//			
-//			while(result.next()) {
-//				System.out.println("------------------------");
-//				System.out.println("Order ID: "+ result.getInt("order_id"));
-//				System.out.println("Username: "+ result.getString("username"));
-//				System.out.println("Product ID: "+ result.getInt("product_id"));
-//				System.out.println("Item: "+ result.getString("item"));
-//				System.out.println("Order Quantity: "+ result.getInt("order_qty"));
-//				System.out.println("Price: "+result.getFloat("price"));
-//			}
-//			System.out.println("------------------------\n");
-//			
-//		}catch(SQLException e) {
-//			e.printStackTrace();
-//		}
-//	}
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-	public int getOrderId() {
-		return orderId;
-	}
+    public int getOrderQty() {
+        return orderQty;
+    }
 
-	public void setOrderId(int orderId) {
-		this.orderId = orderId;
-	}
+    public void setOrderQty(int orderQty) {
+        this.orderQty = orderQty;
+    }
 
-	public int getProductId() {
-		return productId;
-	}
+    public String getUsername() {
+        return username;
+    }
 
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
+    public void setUsername(String username) {
+        this.username = username;
+    }
 
-	public int getOrderQty() {
-		return orderQty;
-	}
+    public String getItem() {
+        return item;
+    }
 
-	public void setOrderQty(int orderQty) {
-		this.orderQty = orderQty;
-	}
+    public void setItem(String item) {
+        this.item = item;
+    }
 
-	public String getUsername() {
-		return username;
-	}
+    public float getPrice() {
+        return price;
+    }
 
-	public void setUsername(String username) {
-		this.username = username;
-	}
-
-	public String getItem() {
-		return item;
-	}
-
-	public void setItem(String item) {
-		this.item = item;
-	}
-
-	public float getPrice() {
-		return price;
-	}
-
-	public void setPrice(float price) {
-		this.price = price;
-	}
+    public void setPrice(float price) {
+        this.price = price;
+    }
 }
