d00db27b97bb60d45b8df8e91c025a29d3823342
RIYA SHUKLA <114848829+riya0785@users.noreply.github.com>
2024-05-06 11:38:25 +0530

    Update Inventory.java

--- src/inventory/Inventory.java
+++ src/inventory/Inventory.java
@@ -1,5 +1,3 @@
-package inventory;
-
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -7,247 +5,179 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
-import database.DBConnector;
+import database.DataBaseMethods;
 import main.OrganicAuraUI;
 
-public class Inventory extends DBConnector {
+public class Inventory extends DataBaseMethods {
 
-	private int productId, quantity;
-	private float price;
-	private String item;
-	
-	
-	public Inventory(int productId, int quantity, float price, String item) {
-		this.productId = productId;
-		this.quantity = quantity;
-		this.price = price;
-		this.item = item;
-	}
+    private int productId, quantity;
+    private float price;
+    private String item;
 
-	public Inventory() {};
+    // Constructor to initialize product details
+    public Inventory(int productId, int quantity, float price, String item) {
+        this.productId = productId;
+        this.quantity = quantity;
+        this.price = price;
+        this.item = item;
+    }
 
-	public void read() {
-		Connection connect = connectInventoryDB();
-		
-		String query = "SELECT * FROM product WHERE product_id = ?";
-		
-		ResultSet result = null;
-		
-		try (PreparedStatement st = connect.prepareStatement(query)){
-			
-			st.setInt(1, getProductId());
-			
-			result = st.executeQuery();
-			
-			if(result.next()) {
-				productId = result.getInt("product_id");
-				quantity = result.getInt("quantity");
-				price = result.getFloat("price");
-				item = result.getString("item");
-			}
-			
-			
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-		}
-		OrganicAuraUI ui = new OrganicAuraUI();
+    // Empty constructor
+    public Inventory() {};
+
+    // Method to read product details from the database
+    public void read() {
+        Connection connect = connectInventoryDB();
+        String query = "SELECT * FROM product WHERE product_id = ?";
+        ResultSet result = null;
+        try (PreparedStatement st = connect.prepareStatement(query)){
+            st.setInt(1, getProductId());
+            result = st.executeQuery();
+            if(result.next()) {
+                productId = result.getInt("product_id");
+                quantity = result.getInt("quantity");
+                price = result.getFloat("price");
+                item = result.getString("item");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        // Refresh inventory table in the UI
+        OrganicAuraUI ui = new OrganicAuraUI();
         ui.refreshInventoryTable();
-		
-	}
+    }
 
-	public void add() {
-		Connection connect = connectInventoryDB();
+    // Method to add a new product to the database
+    public void add() {
+        Connection connect = connectInventoryDB();
+        String query = "INSERT into product (product_id, item, quantity, price) VALUES (?,?,?,?)";
+        try (PreparedStatement st = connect.prepareStatement(query)) {
+            st.setInt(1, getProductId());
+            st.setString(2, getItem());
+            st.setInt(3, getQuantity());
+            st.setFloat(4, getPrice());
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Successfully Added");
+            } else {
+                System.out.println("Addition Failed");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-		String query = "INSERT into product (product_id, item, quantity, price) VALUES (?,?,?,?)";
+    // Method to update product details in the database
+    public void update() {
+        Connection connect = connectInventoryDB();
+        String query = "UPDATE product SET item = ?, price = ?, quantity = ? WHERE product_id = ?";
+        try (PreparedStatement st = connect.prepareStatement(query)) {
+            st.setString(1, getItem());
+            st.setFloat(2, getPrice());
+            st.setInt(3, getQuantity());
+            st.setInt(4, getProductId());
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Successfully Updated");
+            } else {
+                System.out.println("Update Failed");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
 
-		try (PreparedStatement st = connect.prepareStatement(query)) {
+    // Method to delete a product from the database
+    public void delete() {
+        Connection connect = connectInventoryDB();
+        String query = "DELETE FROM product WHERE product_id = ?";
+        try (PreparedStatement st = connect.prepareStatement(query)) {
+            st.setInt(1, getProductId());
+            int rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Product deleted successfully.");
+            } else {
+                System.out.println("No product found with the specified ID.");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    // Method to retrieve all products from the database
+    public List<Object[]> showProducts() {
+        List<Object[]> productList = new ArrayList<>();
+        Connection connect = connectInventoryDB();
+        String query = "SELECT * FROM product";
+        try (PreparedStatement st = connect.prepareStatement(query);
+             ResultSet result = st.executeQuery()) {
+            while (result.next()) {
+                int productId = result.getInt("product_id");
+                String item = result.getString("item");
+                int quantity = result.getInt("quantity");
+                float price = result.getFloat("price");
+                
+                //Create an array to store the product details
+                Object[] row = {productId, item, quantity, price};
+                productList.add(row);
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return productList;
+    }
 
-			st.setInt(1, getProductId());
-			st.setString(2, getItem());
-			st.setInt(3, getQuantity());
-			st.setFloat(4, getPrice());
+    // Method to deduct a quantity from the product stock
+    public int deduct(int reduction) {
+        Connection connect = connectInventoryDB();
+        String query = "UPDATE product SET quantity = quantity - ? WHERE product_id = ?";
+        int rowsAffected = 0;
+        try (PreparedStatement st = connect.prepareStatement(query)) {
+            st.setInt(1, reduction);
+            st.setInt(2, getProductId());
+            rowsAffected = st.executeUpdate();
+            if (rowsAffected > 0) {
+                System.out.println("Successfully Updated");
+            } else {
+                System.out.println("Update Failed");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return rowsAffected;
+    }
 
-			int rowsAffected = st.executeUpdate();
+    // Getters & Setters
 
-			if (rowsAffected > 0) {
-				System.out.println("Successfully Added");
-			} else {
-				System.out.println("Addition Failed");
-			}
+    public int getProductId() {
+        return productId;
+    }
 
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
+    public void setProductId(int productId) {
+        this.productId = productId;
+    }
 
-	public void update() {
-		Connection connect = connectInventoryDB();
+    public int getQuantity() {
+        return quantity;
+    }
 
-		String query = "UPDATE product " + "SET item = ?, price = ?, quantity = ? " + "WHERE product_id = ?";
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
 
-		try (PreparedStatement st = connect.prepareStatement(query)) {
+    public float getPrice() {
+        return price;
+    }
 
-			st.setString(1, getItem());
-			st.setFloat(2, getPrice());
-			st.setInt(3, getQuantity());
-			st.setInt(4, getProductId());
+    public void setPrice(float price) {
+        this.price = price;
+    }
 
-			int rowsAffected = st.executeUpdate();
+    public String getItem() {
+        return item;
+    }
 
-			if (rowsAffected > 0) {
-				System.out.println("Successfully Updated");
-			} else {
-				System.out.println("Update Failed");
-			}
-
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-		
-	}
-
-
-	public void delete() {
-
-		Connection connect = connectInventoryDB();
-
-		// Query
-		String query = "DELETE FROM product WHERE product_id = ?";
-
-		try (
-				// Prepare Query Statement
-				PreparedStatement st = connect.prepareStatement(query);) {
-			// Setting product ID to Query Statement
-			st.setInt(1, getProductId());
-
-			// Execute the delete statement
-			int rowsAffected = st.executeUpdate();
-
-			if (rowsAffected > 0) {
-				System.out.println("Product deleted successfully.");
-			} else {
-				System.out.println("No product found with the specified ID.");
-			}
-		} catch (SQLException e) {
-			e.printStackTrace();
-		}
-
-	}
-	
-	public List<Object[]> showProducts() {
-	    List<Object[]> productList = new ArrayList<>();
-	    Connection connect = connectInventoryDB();
-	    String query = "SELECT * FROM product";
-
-	    try (PreparedStatement st = connect.prepareStatement(query);
-	         ResultSet result = st.executeQuery()) {
-	        while (result.next()) {
-	            int productId = result.getInt("product_id");
-	            String item = result.getString("item");
-	            int quantity = result.getInt("quantity");
-	            float price = result.getFloat("price");
-
-	            Object[] row = {productId, item, quantity, price};
-	            productList.add(row);
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-
-	    return productList;
-	}
-
-//	public ResultSet show() {
-//
-//		Connection connect = connectInventoryDB();
-//		String query = "SELECT * FROM product";
-//
-//		ResultSet result = null;
-//
-//		try (PreparedStatement st = connect.prepareStatement(query)) {
-//			result = st.executeQuery();
-			
-//			while (result.next()) {
-
-//				productId = result.getInt("product_id");
-//				item = result.getString("item");
-//				price = result.getFloat("price");
-//				quantity = result.getInt("quantity");
-//
-//				System.out.println("-----------------------------");
-//				System.out.println("Product ID: " + productId);
-//				System.out.println("Product Name: " + item);
-//				System.out.println("Price: " + price);
-//				System.out.println("Quantity in Stock: " + quantity);
-//				System.out.println("-----------------------------");
-//			}
-//
-//		} catch (SQLException e) {
-//			e.printStackTrace();
-//		}
-//		return result;
-//		
-//	}
-	
-	public int deduct(int reduction) {
-	    Connection connect = connectInventoryDB();
-	    String query = "UPDATE product SET quantity = quantity - ? WHERE product_id = ?";
-	    
-	    int rowsAffected = 0;
-	    
-	    try (PreparedStatement st = connect.prepareStatement(query)) {
-	        st.setInt(1, reduction);
-	        st.setInt(2, getProductId());
-
-	        rowsAffected = st.executeUpdate();
-
-	        if (rowsAffected > 0) {
-	            System.out.println("Successfully Updated");
-	        } else {
-	            System.out.println("Update Failed");
-	        }
-	    } catch (SQLException e) {
-	        e.printStackTrace();
-	    }
-	    return rowsAffected;
-	}
-
-
-//	------------------------Getters & Setters --------------------------
-
-	public int getProductId() {
-		return productId;
-	}
-
-	public void setProductId(int productId) {
-		this.productId = productId;
-	}
-
-	public int getQuantity() {
-		return quantity;
-	}
-
-	public void setQuantity(int quantity) {
-		this.quantity = quantity;
-	}
-
-	public float getPrice() {
-		return price;
-	}
-
-	public void setPrice(float price) {
-		this.price = price;
-	}
-
-	public String getItem() {
-		return item;
-	}
-
-	public void setItem(String item) {
-		this.item = item;
-	}
-
-	
+    public void setItem(String item) {
+        this.item = item;
+    }
 }
